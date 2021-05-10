cfda11f07929daaf3aa8ae97e53b115ae01cf3ed
12metalx <perezozo79@gmail.com>
2021-05-10 17:32:06 -0500

    Intento de pintar la matriz de botones

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,49 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Controls from "./modules/Controls"
 import Help from "./modules/Help"
+import Panel from "./modules/Panel"
 import Scoreboard from "./modules/Scoreboard"
 import Settings from "./modules/Settings"
 
-
+interface Cell{
+    state:number;
+    outbreaks:number;
+}
 const App = () => {
     const [settingsView, setSettingsView] = useState<boolean>(false)
     const [helpView, setHelpView] = useState<boolean>(false)
     const [heigth, setHeigth] = useState<number>(9)
     const [width, setWidth] = useState<number>(9)
     const [outbreaks, setOutbreaks] = useState<number>(10)
+    const [matrix, setMatrix] = useState<Cell[][]>([])
+    useEffect( () => {
+        return () => {
+            
+            setMatrix([])
+
+            
+        }
+    }, [heigth,width,outbreaks])
+    useEffect(() => {
+ 
+        return () => {
+
+            if(matrix.length === 0){
+                var i:number
+                var j:number
+                const row:Cell[] = [];
+                const cell:Cell = {state:0,outbreaks:0}
+                const mat:Cell[][] = [[]]
+                for(j=0;j<width;j++){
+                    row.push(cell)
+                }
+                for(i=0;i<heigth;i++){
+                   mat.push(row)
+                }
+                setMatrix(mat)
+            }  
+        }
+    }, [matrix])
     return (
         <>
         <Scoreboard outbreaks={outbreaks}/>
@@ -27,7 +60,9 @@ const App = () => {
         setOutbreaks = {setOutbreaks}
         outbreaks = {outbreaks}/> : <></>}
         {helpView ? <Help/> : <></>}
+        <Panel setMatrix ={setMatrix} matrix={matrix}/>
         </>
+        
     )
 }
 
--- src/modules/Panel.tsx
+++ src/modules/Panel.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
-const Panel = () => {
+interface Cell{
+    state:number;
+    outbreaks:number;
+}
+interface Props{
+    setMatrix: (mat:Cell[][]) => void;
+    matrix:Cell[][];
+}
+const Panel = ({setMatrix,matrix}:Props) => {
     return (
         <main>
-            
+            {matrix.map((cells:Cell[])=>{return(
+                cells.map((cell:Cell)=>{
+                    return(
+                        <button>asasas</button>
+                    )
+                })
+            )})}
         </main>
     )
 }
